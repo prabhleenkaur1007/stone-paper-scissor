ae1aaaee72641ff160ca889f73f8ff007274e8a3
prabhleenkaur1007 <43867458+prabhleenkaur1007@users.noreply.github.com>
2019-01-22 20:50:19 +0530

    Update stone-paper-scissor.py

--- stone-paper-scissor.py
+++ stone-paper-scissor.py
@@ -1,21 +1,9 @@
 #let's play a game!!!
-user1=str(input("user1: "))
-user2=str(input("user2: "))
-
-
-if user1 == "stone"and user2 == "stone"or user1=="scissor"and user2=="scissor"or user1=="paper"and user2=="paper":
-    print ("result is draw")
-
-elif user1=="paper"and user2=="stone"or user1=="stone"and user2=="scissor"or user1=="scissor"and user2=="paper":
-    print ("user1 wins")
-
-else:
-    print ("user2 wins")#let's play a game!!!
 score1= 0
 score2=0
 for x in range (5):
-  user1=str(input("user1: "))
-  user2=str(input("user2: "))
+  user1=str(input("user1: ")).split().lower()
+  user2=str(input("user2: ")).split().lower()
 
 
   if user1 == "stone"and user2 == "stone"or user1=="scissor"and user2=="scissor"or user1=="paper"and user2=="paper":
@@ -24,10 +12,11 @@ for x in range (5):
   elif user1=="paper"and user2=="stone"or user1=="stone"and user2=="scissor"or user1=="scissor"and user2=="paper":
       print ("user1 wins")
       score1+=1
-
-  else:
+  elif user1=="stone" and user2=="paper" or user1=="scissor"and user2=="stone" or user1=="paper" and user2=="scissor":
       print ("user2 wins")
       score2+=1
-print(score1,score2)
+  else:
+      print("invalid input!!!")
+print("scores!:",score1,score2)
 
 
